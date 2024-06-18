895aefc8824fddb65a4f7bc25880a570096d616e
Owain15 <153022268+Owain15@users.noreply.github.com>
2024-06-19 00:51:28 +0100

    Card Rules Wighten. Need To Add A Tie Break Method. Wright Tests For All Card Hand Rules.

---
+++ Owain.PokerHands/HandRules.cs
@@ -0,0 +1,258 @@
+﻿
+using System.Threading;
+
+public class RoyalFlushHandRule : IHandRule
+{
+    public bool isTrue(List<Card> hand)
+    {
+        var suit = hand[0].Suit;
+
+        var isSingleSuit = hand.All(card => card.Suit == suit);
+
+        if (!isSingleSuit)
+        {
+            return false;
+        }
+
+        var hasFiveValidCards = hand.Count(card => card.Name == 'T'
+        || card.Name == 'J'
+        || card.Name == 'Q'
+        || card.Name == 'K'
+        || card.Name == 'A'
+        ) == 5;
+
+        if (!hasFiveValidCards)
+        {
+            return false;
+        }
+
+        return true;
+    }
+};
+
+public class StraightFlushHandRule : IHandRule
+{
+    public bool isTrue(List<Card> hand)
+    {
+        var suit = hand[0].Suit;
+
+        var isSingleSuit = hand.All(card => card.Suit == suit);
+
+        if (!isSingleSuit)
+        {
+            return false;
+        }
+
+        var hasAnAce = hand.Any(it => it.Name == 'A');
+        var hasATwo = hand.Any(it => it.Name == '2');
+        if (hasAnAce && hasATwo)
+        {
+            var ace = hand.First(it => it.Name == 'A');
+            ace.Value = 1;
+        }
+
+
+        var gapBetweenHighestAndLowest = hand.Max(it => it.Value) - hand.Min(it => it.Value);
+        if (gapBetweenHighestAndLowest > 4)
+        {
+            return false;
+        }
+
+        return true;
+    }
+};
+
+public class FourOfAKindHandRule : IHandRule
+{
+    public bool isTrue(List<Card> hand)
+    {
+        List<int> cardValues = new List<int>();
+        foreach (var card in hand) 
+        {
+            cardValues.Add(card.Value);
+        }
+
+        cardValues.Sort();
+
+        if (cardValues[0] == cardValues[3] || cardValues[1] == cardValues[4]) 
+        { return true; }
+        else { return false; }
+       
+    }
+};
+
+public class FullHouseHandRule : IHandRule
+{
+    public bool isTrue(List<Card> hand)
+    {
+        var hasThreeOfAKind = false;
+        var hasAPair = false;
+
+        var threeOfAKindValue = 0;
+
+        for (int i = 0; i < hand.Count; i++)
+        {
+            var duplicates = 0;
+
+            var currentCardValue = hand[i].Value;
+
+            foreach (var card in hand)
+            { if (card.Value == currentCardValue) { duplicates++; } }
+
+            if (duplicates == 3)
+            { 
+                hasThreeOfAKind = true; 
+                threeOfAKindValue = currentCardValue;
+                i = hand.Count+1;
+            }
+        }
+            if(!hasThreeOfAKind)
+            { return false; }
+            else 
+            { 
+                List<Card> handCopy = new List<Card>();
+                foreach (var card in hand)
+                { handCopy.Add(card); }
+
+                handCopy.RemoveAll( card => card.Value == threeOfAKindValue);
+
+                handCopy.OrderBy(card => card.Value);
+
+
+                for (int j = 1; j < handCopy.Count; j++)
+                {
+                    if (handCopy[(j - 1)] == handCopy[j])
+                    { hasAPair = true; }
+                }
+
+            }
+
+            if (hasAPair && hasThreeOfAKind) { return true; }
+            else { return false; }
+
+        
+
+        return false;
+
+       // Full House: Three of a kind and a pair
+    }
+};
+
+public class FlushHandRule : IHandRule
+{
+    public bool isTrue(List<Card> hand)
+    {
+        var suit = hand[0].Suit;
+
+        var isSingleSuit = hand.All(card => card.Suit == suit);
+
+        if (!isSingleSuit)
+        {
+            return false;
+        }
+        else { return true; }
+        
+    }
+};
+
+public class StraightHandRule : IHandRule
+{
+    public bool isTrue(List<Card> hand)
+    {
+        var handInAscendingOrder = hand.OrderBy(card => card.Value);
+
+        int min = handInAscendingOrder.First().Value;
+        var max = handInAscendingOrder.Last().Value;
+
+        var isAStraight = (max - min) == (handInAscendingOrder.Count() - 1);
+
+        if (!isAStraight) { return false; }
+        else { return true; }
+       
+    }
+};
+
+public class ThreeOfAKindHandRule : IHandRule
+{
+    public bool isTrue(List<Card> hand)
+    {
+        for(int i = 0; i < hand.Count; i++ )
+        {
+            var duplicates = 0;
+
+            var currentCardValue = hand[i].Value;
+
+            foreach (var card in hand)
+            { if (card.Value == currentCardValue) { duplicates++; } }
+
+            if(duplicates >= 3) { return true; }
+
+        }
+
+        return false;
+    }
+};
+
+public class TwoPairsHandRule : IHandRule
+{
+    public bool isTrue(List<Card> hand)
+    {
+        var pairCount = 0;
+
+        List<int> cardValues = new List<int>();
+
+        foreach (Card card in hand)
+        { 
+         cardValues.Add(card.Value);
+        }
+
+        cardValues.Sort();
+
+        for (int i = 1; i < cardValues.Count; i++)
+        {
+            if(cardValues[(i-1)] == cardValues[i])
+            { pairCount++; }
+        }
+
+        if(pairCount >= 2)
+        { return true; }
+        else { return false; }
+        
+    }
+};
+
+public class OnePairHandRule : IHandRule
+{
+    public bool isTrue(List<Card> hand)
+    {
+        List<int> cardValues = new List<int>();
+
+        foreach (Card card in hand)
+        {
+            cardValues.Add(card.Value);
+        }
+
+        cardValues.Sort();
+
+        for (int i = 1; i < cardValues.Count; i++)
+        {
+            if (cardValues[(i - 1)] == cardValues[i])
+            { return true; }
+        }
+
+        return false;
+    }
+};
+
+
+// may not be necasery. 
+
+//public class HighCardHandRule : IHandRule
+//{
+//    public bool isTrue(List<Card> hand)
+//    {
+
+
+//        return false;
+//    }
+//};
--- Owain.PokerHands/Program.cs
+++ Owain.PokerHands/Program.cs
@@ -1,8 +1,24 @@
-﻿//  load the file
+﻿
 using Poker;
+
+
 
 var royalFlushRuleProcessor = new RoyalFlushHandRule();
 var straightFlushRuleProcessor = new StraightFlushHandRule();
+var fourOfAKindProcessor = new FourOfAKindHandRule();
+var fullHouseProcessor = new FullHouseHandRule();
+var flushProcessor = new FlushHandRule();
+var StraightProcessor = new StraightHandRule();
+var threeOfAKindProcessor = new ThreeOfAKindHandRule();
+var twoPairsProcessor = new TwoPairsHandRule();
+var onePairProcessor = new OnePairHandRule();
+//var highCardProcessor = new HighCardHandRule();
+
+var roundNumber = 1;
+var p1Score = 0;
+var p2Score = 0;
+
+//  load the file
 
 var lines = File.ReadAllText("./poker.txt")
     .Split("\r\n")
@@ -25,40 +41,112 @@ foreach (var line in lines)
     var leftHandScore = GetScore(leftHand.Select(it => it.Card).ToList());
     var rightHandScore = GetScore(rightHand.Select(it => it.Card).ToList());
 
+    var roundResult = "Result Not Found.";
+
     if (leftHandScore > rightHandScore)
     {
-        Console.WriteLine("player one wins");
+        p1Score++;
+        roundResult="Player One Wins.";
     }
 
     if (leftHandScore < rightHandScore)
     {
-        Console.WriteLine("player two wins");
+        p2Score++;
+        roundResult = "Player Two wins.";
     }
 
     if (leftHandScore == rightHandScore)
     {
-        Console.WriteLine("draw");
+        roundResult = "Draw.";
     }
+
+    Console.WriteLine();
+    Console.WriteLine($" Round:{roundNumber}   ||   {roundResult}");
+    Console.WriteLine();
+
+    roundNumber++;
+
+}
+
+DisplayResult();
+
+void DisplayResult()
+{
+    Console.WriteLine();
+    Console.WriteLine("   .......... .......... .......... .......... ..........   ");
+    Console.WriteLine();
+    Console.WriteLine($"       Player One Score: {p1Score}  ||   Player Two Score: {p2Score}  ");
+    Console.WriteLine();
+    Console.WriteLine("   .......... .......... .......... .......... ..........   ");
+    Console.WriteLine();
 }
 
 Score GetScore(List<Card> hand)
 {
-    Console.WriteLine(hand);
+    //Console.WriteLine(hand);
 
     if (royalFlushRuleProcessor.isTrue(hand))
     {
-        Console.WriteLine("royal flush!");
+        Console.WriteLine("Royal Flush!");
         return Score.RoyalFlush;
     }
     else if ( straightFlushRuleProcessor.isTrue(hand))
     {
-        Console.WriteLine("straight flush!");
+        Console.WriteLine("Straight Flush!");
         return Score.StraightFlush;
     }
+    else if (fourOfAKindProcessor.isTrue(hand)) 
+    {
+        Console.WriteLine("Four Of A Kind!");
+        return Score.FourOfAKind;
 
+    }
+    else if (fullHouseProcessor.isTrue(hand))
+    {
+        Console.WriteLine("Full House!");
+        return Score.FullHouse;
 
-    Console.WriteLine("No hand found");
-    return 0;
+    }
+    else if (flushProcessor.isTrue(hand))
+    {
+        Console.WriteLine("Flush!");
+        return Score.Flush;
+
+    }
+    else if (StraightProcessor.isTrue(hand))
+    {
+        Console.WriteLine("Straight!");
+        return Score.Straight;
+
+    }
+    else if (threeOfAKindProcessor.isTrue(hand))
+    {
+        Console.WriteLine("Three Of A Kind!");
+        return Score.ThreeOfAKind;
+
+    }
+    else if (twoPairsProcessor.isTrue(hand))
+    {
+        Console.WriteLine("Two Pairs!");
+        return Score.TwoPairs;
+
+    }
+    else if (onePairProcessor.isTrue(hand))
+    {
+        Console.WriteLine("A Pair!");
+        return Score.OnePair;
+
+    }
+    else 
+    {
+        Console.WriteLine("High Card!");
+        return Score.HighCard;
+
+    }
+
+    //Console.WriteLine("No hand found");
+    //return 0;
+   
     //throw new Exception("Hand not found!");
 }
 
